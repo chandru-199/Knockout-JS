fe6c2e8468f9dc164c190f38769895bff8016a63
chandru-199 <51230988+chandru-199@users.noreply.github.com>
2019-06-12 15:29:23 +0530

    Update table1.js

--- table1.js
+++ table1.js
@@ -1,4 +1,4 @@
-define('table1', ['jquery'], function ($) {
+define('table1', function () {
     return {
         addrow: function () {
             var row1 = $('<tr><td><input type="text" name="p_name"></td><td><input type="text" name="p_skew"></td><td><input type="number" name="quantity"></td><td><input type="text" name="price"></td><td><button id="remove">Remove</button></td></tr>');
